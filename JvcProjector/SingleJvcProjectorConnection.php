8c5d13fc1d02cce05ed5284abe7510501824983d
bernd70 <bernd@family-holz.de>
2022-10-12 12:46:59 +0200

    Semaphore Handling verbessert

--- JvcProjector/SingleJvcProjectorConnection.php
+++ JvcProjector/SingleJvcProjectorConnection.php
@@ -6,17 +6,41 @@ include "JvcProjectorConnection.php";
 
 class SingleJvcProjectorConnection extends JvcProjectorConnection
 {
+    function __destruct()
+    {
+        $this->Disconnect();
+    }
+
     public function Connect()
     {
         if (!IPS_SemaphoreEnter("JVCProjectorConnection", 5000))
             throw new Exception("Es besteht bereits eine Verbindung zum Projektor (Semaphore belegt)");
 
-        parent::Connect();
+        try
+        {
+            parent::Connect();
+        }
+        catch (Exception $e)
+        {
+            IPS_SemaphoreLeave("JVCProjectorConnection");
+
+            throw $e;
+        }
+
     }
 
     public function Disconnect()
     {
-        parent::Disconnect();
+        try
+        {
+            parent::Disconnect();
+        }
+        catch (Exception $e)
+        {
+            IPS_SemaphoreLeave("JVCProjectorConnection");
+
+            throw $e;
+        }
 
         IPS_SemaphoreLeave("JVCProjectorConnection");
     }
